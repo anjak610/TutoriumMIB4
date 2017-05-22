77871b67110ac6a229d76d0510e5bad15c0b91d9
anjak610 <anja.siegrid.kretschmer@hs-furtwangen.de>
2017-05-22 21:58:34 +0200

    added linked List

---
+++ Tut_MIB4/src/LinkedList/ListElem.java
@@ -0,0 +1,38 @@
+package LinkedList;
+
+ class ListElement {
+	
+	Object obj;
+	ListElement nextElem, prevElem;
+	
+	public ListElement(Object obj)
+	{
+		this.obj = obj;
+		nextElem = null;
+	}
+	
+	public void setNextElem(ListElement nextElem)
+	{
+		this.nextElem = nextElem;
+	}
+	
+	public void setPrevElem(ListElement prevElem)
+	{
+		this.prevElem = prevElem;
+	}
+	
+	public ListElement getNextElem()
+	{
+		return nextElem;
+	}
+	
+	public ListElement getPrevElem()
+	{
+		return prevElem;
+	}
+	
+	public Object getObj()
+	{
+		return obj;
+	}
+}
